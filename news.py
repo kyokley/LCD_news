984f565e93f85707a34fe9c54674e2e6fad771db
Kevin Yokley <kyokley2@gmail.com>
2016-12-22 22:14:01 -0600

    Adding news module

---
+++ news.py
@@ -0,0 +1,18 @@
+import requests
+import feedparser
+
+SOURCES = ['http://feeds.feedburner.com/TheHackersNews']
+
+def get_headlines():
+    headlines = []
+    for source in SOURCES:
+        try:
+            data = requests.get(source, timeout=1).content
+        except:
+            continue
+
+        feed_data = feedparser.parse(data)
+        headlines.extend([x.get('title_detail', {})
+                           .get('value', {})
+                           for x in feed_data.entries])
+    return headlines
